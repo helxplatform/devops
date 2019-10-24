6cd7ccb0cd50698f1b311c40eba9cf098527c81f
PJ Linebaugh <pjl@renci.org>
2019-10-23 22:43:51 -0400

    Added methods for deploying CommonsShare and Tycho.  Can now add an accelerator to the base cluster nodes.  These worked at one point, but my latest tries the GPU only shows on when viewing the details of the VM.  Added some notes and left a few lines that can be commented out later if they aren't needed.  Set the disk that is created to have the same prefix of the auto-created disks for the clusters.  Added a way to source a config file of variables rather than set them in the calling shell.

--- gke/bin/AdminStageGKEKubeCluster.sh
+++ gke/bin/AdminStageGKEKubeCluster.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 # This bash script shall create a GKE cluster, an external IP, setup kubectl to
 # connect to the cluster without changing the home kube config and finally installs
 # helm with the appropriate service account if RBAC is enabled
@@ -24,17 +24,31 @@
 # * Delete all old GKE clusters.
 #
 
+# To override the variables below you can can export them out in a file and
+# then set the variable "GKE_CLUSTER_CONFIG" to the location of that file.
+if [ -z "${!GKE_CLUSTER_CONFIG}" ]
+then
+  source ${GKE_CLUSTER_CONFIG}
+fi
+
 set -e
-PROJECT=${PROJECT-stage-mvp}
-REGION=${REGION-us-east4}
+#
+# These variables can be set outside of this script.
+#
+#PROJECT=${PROJECT-stage-mvp}
+#PROJECT=${PROJECT-research-237918}
+PROJECT=${PROJECT-stack-dev-237116}
+REGION=${REGION-us-east1}
 ZONE_EXTENSION=${ZONE_EXTENSION-b}
-ZONE=${REGION}-${ZONE_EXTENSION}
 CLUSTER_ENV=${CLUSTER_ENV-dev}
 CLUSTER_NAME=${CLUSTER_NAME-stage-${CLUSTER_ENV}}
-CLUSTER_VERSION=${CLUSTER_VERSION-1.13.6-gke.13}
-MACHINE_TYPE=${MACHINE_TYPE-n1-standard-8}
-ACCELERATOR_TYPE=${ACCELERATOR_TYPE-""}
-ACCELERATOR_COUNT=${ACCELERATOR_COUNT-1}
+CLUSTER_VERSION=${CLUSTER_VERSION-1.13.7-gke.24}
+MACHINE_TYPE=${MACHINE_TYPE-n1-standard-2}
+ADD_CLUSTER_ACCELERATOR=${ADD_CLUSTER_ACCELERATOR-false}
+CLUSTER_ACCELERATOR_TYPE=${CLUSTER_ACCELERATOR_TYPE-"nvidia-tesla-p100"}
+CLUSTER_ACCELERATOR_COUNT=${CLUSTER_ACCELERATOR_COUNT-1}
+NP_ACCELERATOR_TYPE=${NP_ACCELERATOR_TYPE-"nvidia-tesla-p100"}
+NP_ACCELERATOR_COUNT=${NP_ACCELERATOR_COUNT-1}
 RBAC_ENABLED=${RBAC_ENABLED-true}
 NUM_NODES=${NUM_NODES-2}
 MIN_NODES=${MIN_NODES-0}
@@ -46,14 +60,25 @@ INT_NETWORK=${INT_NETWORK-default}
 PREEMPTIBLE=${PREEMPTIBLE-false}
 EXTRA_CREATE_ARGS=${EXTRA_CREATE_ARGS-""}
 USE_STATIC_IP=${USE_STATIC_IP-false}
-external_ip_name=${CLUSTER_NAME}-external-ip
 HELIUM_GKE_HOME=${HELIUM_GKE_HOME-$(pwd)/../..}
-# NFS_STORAGE_NAME also needs to be changed in the NFS server YAML.
-NFS_STORAGE_NAME=${NFS_STORAGE_NAME-gce-nfs-disk}
+# NFS_STORAGE_NAME also needs to be changed in the NFS server YAML.  Currently
+# this is done via an env variable in the YAML.
+NFS_STORAGE_NAME=${NFS_STORAGE_NAME-gke-${CLUSTER_NAME}-elk-pd}
 NFS_STORAGE_SIZE=${NFS_STORAGE_SIZE-10GB}
-KUBECONFIG_DIR=${HELIUM_GKE_HOME}/kubeconfigs/${PROJECT}-${CLUSTER_NAME}
-KUBECONFIG=${KUBECONFIG_DIR}/config
+COMMONSSHARE_K8S=${COMMONSSHARE_K8S-${HELIUM_GKE_HOME}/../../../heliumdatacommons/commonsshare/commonsshare/k8s}
+TYCHO_K8S=${TYCHO_K8S-${HELIUM_GKE_HOME}/../../../heliumplus/tycho/tycho/kubernetes}
+KUBECONFIG_DIR=${KUBECONFIG_DIR-${HELIUM_GKE_HOME}/kubeconfigs/${PROJECT}-${CLUSTER_NAME}}
+# POSTGIS_STORAGE_NAME=${POSTGIS_STORAGE_NAME-gke-${CLUSTER_NAME}-postgis-pd}
+# POSTGIS_STORAGE_SIZE=${POSTGIS_STORAGE_SIZE-1GB}
+
+# end setable variables
+
+ZONE=${REGION}-${ZONE_EXTENSION}
+USER_KUBECONFIG=$KUBECONFIG
+SCRIPT_KUBECONFIG=${KUBECONFIG_DIR}/config
+KUBECONFIG=${SCRIPT_KUBECONFIG}
 KUBECONFIG_USER=${PROJECT}-${CLUSTER_NAME}-admin-user
+external_ip_name=${CLUSTER_NAME}-external-ip
 
 # MacOS does not support readlink, but it does have perl
 KERNEL_NAME=$(uname -s)
@@ -79,12 +104,25 @@ function bootstrap(){
     EXTRA_CREATE_ARGS="$EXTRA_CREATE_ARGS --preemptible"
   fi
 
+  if ${ADD_CLUSTER_ACCELERATOR}; then
+    echo "Adding accelerator to base cluster."
+    EXTRA_CREATE_ARGS="$EXTRA_CREATE_ARGS --accelerator type=${CLUSTER_ACCELERATOR_TYPE},count=${CLUSTER_ACCELERATOR_COUNT}"
+  else
+    echo "Not adding accelerator to base cluster."
+  fi
+
   gcloud container clusters create $CLUSTER_NAME --zone $ZONE \
     --cluster-version $CLUSTER_VERSION --machine-type $MACHINE_TYPE \
-    --scopes "https://www.googleapis.com/auth/ndev.clouddns.readwrite","https://www.googleapis.com/auth/compute","https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
+    --scopes "https://www.googleapis.com/auth/ndev.clouddns.readwrite",\
+"https://www.googleapis.com/auth/compute",\
+"https://www.googleapis.com/auth/devstorage.read_only",\
+"https://www.googleapis.com/auth/logging.write",\
+"https://www.googleapis.com/auth/monitoring",\
+"https://www.googleapis.com/auth/servicecontrol",\
+"https://www.googleapis.com/auth/service.management.readonly",\
+"https://www.googleapis.com/auth/trace.append" \
     --node-version $CLUSTER_VERSION --num-nodes $NUM_NODES \
-    --enable-ip-alias \
-    --network $INT_NETWORK \
+    --enable-ip-alias --network $INT_NETWORK \
     --project $PROJECT --enable-basic-auth $EXTRA_CREATE_ARGS;
 
   if ${USE_STATIC_IP}; then
@@ -95,11 +133,18 @@ function bootstrap(){
     echo "#####\n"
   fi
 
+  # Save new cluster kubeconfig in local directory.
   mkdir -p $KUBECONFIG_DIR
   # Erase k8s config if there.
   echo "" > $KUBECONFIG_DIR/config
-
   gcloud container clusters get-credentials $CLUSTER_NAME --zone $ZONE --project $PROJECT;
+
+  # Add new cluster kubeconfig to user's kubeconfig.
+  KUBECONFIG=$USER_KUBECONFIG
+  gcloud container clusters get-credentials $CLUSTER_NAME --zone $ZONE --project $PROJECT;
+  KUBECONFIG=$SCRIPT_KUBECONFIG
+  # add a 'user' in user's kubeconfig
+  kubectl --kubeconfig $USER_KUBECONFIG config set-credentials ${KUBECONFIG_USER} --username=admin --password=$(cluster_admin_password_gke)
 
   # Create roles for RBAC Helm
   if $RBAC_ENABLED; then
@@ -110,18 +155,23 @@ function bootstrap(){
       exit 1;
     fi
 
-    kubectl config set-credentials ${KUBECONFIG_USER} --username=admin --password=$(cluster_admin_password_gke)
-    kubectl create --user=${KUBECONFIG_USER} -f rbac-config.yaml;
+    # kubectl config set-credentials ${KUBECONFIG_USER} --username=admin --password=$(cluster_admin_password_gke)
+    # kubectl create --user=${KUBECONFIG_USER} -f rbac-config.yaml;
+    kubectl create -f rbac-config.yaml;
+  fi
+
+  if ${ADD_CLUSTER_ACCELERATOR}; then
+    installAcceleratorDaemonset
   fi
 
   # deploy tiller here?
 
-  echo "###"
+  echo "######"
   echo "For kubectl configuration of this cluster only:"
   echo "  export KUBECONFIG=$KUBECONFIG"
   echo "To add to your own kubectl configurations use:"
   echo "  export KUBECONFIG=\$KUBECONFIG:$KUBECONFIG"
-  echo "###"
+  echo "######"
 }
 
 
@@ -131,15 +181,20 @@ function cleanup_gke_resources(){
   gcloud container clusters delete -q $CLUSTER_NAME --zone $ZONE --project $PROJECT;
   echo "Deleted $CLUSTER_NAME cluster successfully";
   if ${USE_STATIC_IP}; then
-    gcloud compute addresses delete -q $external_ip_name --region $REGION --project $PROJECT;
+    gcloud compute addresses delete -q $external_ip_name --region $REGION --project $PROJECT &&
     echo "Deleted ip: $external_ip_name successfully";
   fi
-  echo "\033[;33m Warning: Disks, load balancers, DNS records, and other cloud resources created during the helm deployment are not deleted, please delete them manually from the gcp console \033[0m";
+  echo "######"
+  echo "Warning: Disks, load balancers, DNS records, and other cloud resources"
+  echo "created during the deployment are not deleted, please delete them"
+  echo "manually from the gcp console.  Check disks that start with:"
+  echo "  gke-${CLUSTER_NAME}-"
+  echo "######";
 }
 
 function createNodePool(){
-   if ! [ -z "${ACCELERATOR_TYPE}" ]; then
-     EXTRA_CREATE_ARGS="--accelerator type=${ACCELERATOR_TYPE},count=${ACCELERATOR_COUNT}"
+   if ! [ -z "${NP_ACCELERATOR_TYPE}" ]; then
+     EXTRA_CREATE_ARGS="--accelerator type=${NP_ACCELERATOR_TYPE},count=${NP_ACCELERATOR_COUNT}"
    fi
 
    echo "creating NodePool with name $1"
@@ -150,11 +205,16 @@ function createNodePool(){
    --max-nodes ${MAX_POOL_NODES} --enable-autoscaling \
    --machine-type ${MACHINE_TYPE} --node-labels=pool-name=$1
 
-   if ! [ -z "${ACCELERATOR_TYPE}" ]; then
-     sleep 15 # Wait a little for nodes to come up.
-     # Deploy Nvidia device drivers to the nodes if an accelerator is used.
-     kubectl apply --user=${KUBECONFIG_USER} -f https://raw.githubusercontent.com/GoogleCloudPlatform/container-engine-accelerators/master/nvidia-driver-installer/cos/daemonset-preloaded.yaml
+   if ! [ -z "${NP_ACCELERATOR_TYPE}" ]; then
+     installAcceleratorDaemonset
    fi
+}
+
+function installAcceleratorDaemonset(){
+  sleep 15 # Wait a little for nodes to come up.
+  # Deploy Nvidia device drivers to the nodes if an accelerator is used.
+  kubectl create -f https://raw.githubusercontent.com/GoogleCloudPlatform/container-engine-accelerators/master/nvidia-driver-installer/cos/daemonset-preloaded.yaml
+  # kubectl create -f https://raw.githubusercontent.com/GoogleCloudPlatform/container-engine-accelerators/stable/daemonset.yaml
 }
 
 function deleteNodePool(){
@@ -166,26 +226,41 @@ function deleteNodePool(){
 function deployELKNFS(){
    echo "deploying ELK and NFS"
    # deploy ELK
-   kubectl apply --user=${KUBECONFIG_USER} -R -f $HELIUM_GKE_HOME/elasticsearch/
-   kubectl apply --user=${KUBECONFIG_USER} -R -f $HELIUM_GKE_HOME/kibana/
-   kubectl apply --user=${KUBECONFIG_USER} -R -f $HELIUM_GKE_HOME/logstash/
+   kubectl apply -R -f $HELIUM_GKE_HOME/elasticsearch/
+   kubectl apply -R -f $HELIUM_GKE_HOME/kibana/
+   kubectl apply -R -f $HELIUM_GKE_HOME/logstash/
 
    # create disk for persistent storage
    gcloud compute disks create $NFS_STORAGE_NAME --size $NFS_STORAGE_SIZE \
           --zone $ZONE --project $PROJECT
    # deploy NFS server
-   kubectl apply --user=${KUBECONFIG_USER} -R -f $HELIUM_GKE_HOME/nfs-server/
+   # deploy all YAML files at once
+   # kubectl apply -R -f $HELIUM_GKE_HOME/nfs-server/
+
+   kubectl apply -R -f $HELIUM_GKE_HOME/nfs-server/nfs-pvc-pv.yaml
+   export NFS_STORAGE_NAME
+   # replace env variable in YAML file NFS_STORAGE_NAME value and deploy inline
+   cat $HELIUM_GKE_HOME/nfs-server/nfs-server.template.yaml | envsubst | kubectl apply -R -f -
+   # kubectl apply -R -f $HELIUM_GKE_HOME/nfs-server/nfs-server.yaml
+   kubectl apply -R -f $HELIUM_GKE_HOME/nfs-server/nfs-service.yaml
 }
 
 function deleteELKNFS(){
    echo "deleting ELK and NFS"
    # delete ELK
-   kubectl delete --user=${KUBECONFIG_USER} -R -f $HELIUM_GKE_HOME/elasticsearch/
-   kubectl delete --user=${KUBECONFIG_USER} -R -f $HELIUM_GKE_HOME/kibana/
-   kubectl delete --user=${KUBECONFIG_USER} -R -f $HELIUM_GKE_HOME/logstash/
+   kubectl delete -R -f $HELIUM_GKE_HOME/elasticsearch/
+   kubectl delete -R -f $HELIUM_GKE_HOME/kibana/
+   kubectl delete -R -f $HELIUM_GKE_HOME/logstash/
 
    # delete NFS server
-   kubectl delete --user=${KUBECONFIG_USER} -R -f $HELIUM_GKE_HOME/nfs-server/
+   # kubectl delete -R -f $HELIUM_GKE_HOME/nfs-server/
+
+   kubectl delete -R -f $HELIUM_GKE_HOME/nfs-server/nfs-pvc-pv.yaml
+   export NFS_STORAGE_NAME
+   cat $HELIUM_GKE_HOME/nfs-server/nfs-server.template.yaml | envsubst | kubectl delete -R -f -
+   # kubectl delete -R -f $HELIUM_GKE_HOME/nfs-server/nfs-server.yaml
+   kubectl delete -R -f $HELIUM_GKE_HOME/nfs-server/nfs-service.yaml
+
    # delete disk for persistent storage
    SLEEP_TIME=30
    echo "Waiting for $SLEEP_TIME seconds for NFS server deletion."
@@ -194,8 +269,26 @@ function deleteELKNFS(){
           --zone $ZONE --project $PROJECT -q
 }
 
+function commonsShare(){
+   echo "executing kubectl $1 on CommonsShare YAMLs"
+   # create/delete disk for persistent storage
+   # if [ $1 == "create" ]; then
+   #   DISKS_EXTRA_OPTIONS="--size $POSTGIS_STORAGE_SIZE"
+   # else
+   #   DISKS_EXTRA_OPTIONS=""
+   # fi
+   # gcloud compute disks $1 $POSTGIS_STORAGE_NAME $DISKS_EXTRA_OPTIONS \
+   #        --zone $ZONE --project $PROJECT -q
+   kubectl $1 -f $COMMONSSHARE_K8S/postgis-claim0-persistentvolumeclaim.yaml,$COMMONSSHARE_K8S/postgis-deployment.yaml,$COMMONSSHARE_K8S/hydroshare-service.yaml,$COMMONSSHARE_K8S/solr-deployment.yaml,$COMMONSSHARE_K8S/hydroshare-env-configmap.yaml,$COMMONSSHARE_K8S/postgis-service.yaml,$COMMONSSHARE_K8S/solr-service.yaml,$COMMONSSHARE_K8S/hydroshare-deployment.yaml
+}
+
+function tycho(){
+   echo "executing kubectl $1 on tycho YAMLs"
+   kubectl $1 -f $TYCHO_K8S/
+}
+
 if [ -z "$1" ]; then
-  echo "Supported commands: createCluster, deleteCluster, createNodePool, deleteNodePool";
+  echo "Supported commands: createCluster, deleteCluster, createNodePool, deleteNodePool, choas, deployELKNFS, deleteELKNFS, createClusterELKNFS, deleteClusterELKNFS, deployCommonsShare, deleteCommonsShare, deployTycho, deleteTycho, createClusterAll, deleteClusterAll";
   exit
 fi
 
@@ -235,6 +328,30 @@ case $1 in
     deployELKNFS;
     ;;
   deleteClusterELKNFS)
+    deleteELKNFS;
+    cleanup_gke_resources;
+    ;;
+  deployCommonsShare)
+    commonsShare create;
+    ;;
+  deleteCommonsShare)
+    commonsShare delete;
+    ;;
+  deployTycho)
+    tycho create;
+    ;;
+  deleteTycho)
+    tycho delete;
+    ;;
+  createClusterAll)
+    bootstrap;
+    deployELKNFS;
+    commonsShare create;
+    tycho create;
+    ;;
+  deleteClusterAll)
+    tycho delete;
+    commonsShare delete;
     deleteELKNFS;
     cleanup_gke_resources;
     ;;
