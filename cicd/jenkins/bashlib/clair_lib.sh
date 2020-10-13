45ad12ce884214a5dd72cfd8e36895dcb0a981a5
Chuck Bennett <cnbennett3@gmail.com>
2020-10-13 13:48:51 -0400

    Lots of rewrite of scan_clair and post-process.

--- cicd/jenkins/bashlib/clair_lib.sh
+++ cicd/jenkins/bashlib/clair_lib.sh
@@ -19,8 +19,11 @@
 # -------------------------------------------------------------------------
 
 function scan_clair () {
-   CLAIR_HM="/var/jenkins_home/clair/"
-   CLAIR_XFM="/var/jenkins_home/clair/xfm" # clair output transform dir
+
+   CLAIR_HM="/var/jenkins_home/clair"
+   CLAIR_XFM="$CLAIR_HM/xfm" # clair output transform dir
+   XFM_DIR="$CLAIR_XFM/$REPO-$BRANCH-$VER"
+
    ORG="$1"
    REPO="$2"
    BRANCH="$3"
@@ -34,23 +37,20 @@ function scan_clair () {
    docker pull $ORG/$REPO:$BRANCH-$VERSION
 
    if [ ! -d "$CLAIR_XFM" ]; then
-      mkdir "$CLAIR_XFM"
+      mkdir $CLAIR_XFM
    fi
 
-   if [ ! -d "$CLAIR_XFM/$BRANCH-$VERSION" ]; then
-      mkdir "$CLAIR_XFM/$BRANCH-$VERSION"
+   if [ ! -d "$XFM_DIR" ]; then
+      mkdir $XFM_DIR
    fi
 
    $CLAIR_HM/clair-scanner --clair=http://$CLAIR_IP:6060 --ip=$ETH0_IP -t 'High' -r \
-      "$CLAIR_XFM/$REPO-$BRANCH-$VERSION/clair_report.json" $ORG/$REPO:$BRANCH-$VERSION > \
-      "$CLAIR_XFM/$REPO-$BRANCH-$VERSION/table.txt"
+      $XFM_DIR/clair_report.json $ORG/$REPO:$BRANCH-$VERSION > $XFM_DIR/table.txt
 
    # Remove control chars
-   grep -o "[[:print:][:space:]]*" "$CLAIR_XFM/$REPO-$BRANCH-$VERSION/table.txt" > \
-        "$CLAIR_XFM/$REPO-$BRANCH-$VERSION/table.txt"
-
-   rm -f "$CLAIR_XFM/$REPO-$BRANCH-$VERSION/table.txt"
-
+   grep -o "[[:print:][:space:]]*" $XFM_DIR/table.txt > \
+                                   $XFM_DIR/clean_table.txt
+   rm -f $XFM_DIR/table.txt
    echo "clair scan complete."
 }
 
@@ -70,7 +70,9 @@ function scan_clair () {
 #        or "" if none
 # $4 - Version number including "v" if any of repo to be
 #        scanned.
-# Example call: postprocess-clair-output "heliumdatastage" "appstore" "develop" "v0.0.13"
+# $5 - Vulnerability threshold. All vulnerabilities at or below this threshold
+#        will be removed from the report.
+# Example call: postprocess-clair-output "heliumdatastage" "appstore" "develop" "v0.0.13" Medium
 # Result:
 #    - Clair json output has Medium and below CVE's removed.
 #    - All URL's are turned into HTML links.
@@ -84,58 +86,57 @@ function postprocess_clair_output() {
    REPO=$2
    BRANCH=$3
    VER=$4
+   THRESHOLD=$5
 
-   CLAIR_DIR=/var/jenkins_home/clair
-   PROJ_DIR=$CLAIR_DIR/$REPO-$BRANCH-$VER
-   WKSPC_DIR=/var/jenkins_home/workspace/
-   LF_DIR="${CTEST:-$REPO}"
-   WK_DIR=$WKSPC_DIR$LF_DIR
+   JENKINS_HM="/var/jenkins_home"
+   CLAIR_HM="$JENKINS_HM/clair"
+   CLAIR_XFM="$CLAIR_HM/xfm"
+   XFM_DIR="$CLAIR_XFM/$REPO-$BRANCH-$VER"
+   CLAIR_RPT="$CLAIR_HM/reports"
+   RPT_DIR="$CLAIR_RPT/$REPO-$BRANCH-$VER"
 
-   # Print lines in awk until the first line that has a Medium threshold
-   # That leaves 9 lines that need to be removed at the end.
    lines_to_remove=9
-   num_lines=$(awk '/Medium/ { exit }
-        { print }
-        END{ print NR }' $PROJ_DIR/clair_report.json | \
-                         tee $PROJ_DIR/clair_report_edited.json | \
-                         wc -l)
+   num_lines=$(awk "              \
+   /unapproved/,/]/  { next }     \
+   /$THRESHOLD/      { exit }     \
+                     { print }    \
+   END               { print NR }" $XFM_DIR/clair_report.json | \
+                               tee $XFM_DIR/clair_report_edited.json | \
+                               wc -l)
 
-   # Note for future development: change clair_report_final below to
-   # clair_report_edited2.json
-   head -n $( expr $num_lines - $lines_to_remove )   \
-                $PROJ_DIR/clair_report_edited.json > \
-                $PROJ_DIR/clair_report_final.json
+   head -n $( expr $num_lines - $lines_to_remove )  \
+                $XFM_DIR/clair_report_edited.json > \
+                $XFM_DIR/clair_report_final.json
 
-   cat >> $PROJ_DIR/clair_report_final.json \
+   # Add back sane JSON ending to file.
+   cat >> $XFM_DIR/clair_report_final.json \
           $CLAIR_DIR/template_json_ending.txt
 
-   # Note for future development: Call python program here to re-order
-   # json list of vulnerability info prior to calling json2table. Also
-   # use clair_report_edited2.json name when calling it, keep "final"
-   # name here.
-   cat $PROJ_DIR/clair_report_final.json | \
+   cat $XFM_DIR/clair_report_final.json | \
                               json2table > \
-                              $PROJ_DIR/clair_html_file.html
+                              $XFM_DIR/clair_table.html
 
    # Convert bare link to an href with CVE as target:
-   # Future Dev:
-   # Note: this href column should be moved to CVE column's place above
-   # and its column deleted.
-   # Note also, the redundant "Unapproved" CVE column should be removed too.
-   sed -i 's|>\(https.*\)\(CVE-.*[0-9]\)<|><a href="\1\2" target="_blank">\2<|g'\
-                              $PROJ_DIR/clair_html_file.html
+   sed -i 's|>\(https.*\)\(CVE-.*[0-9]\)<|><a href="\1\2" target="_blank">\2<|g' \
+                              $XFM_DIR/clair_table.html
 
-   # Remove edited2 file here too.
-   rm $PROJ_DIR/clair_report_edited.json
+   # >>---> INSERT NEW HTML BODY HERE
 
-   cd $WK_DIR
-   if [ ! -d "$WK__DIR/html" ]; then
-      mkdir $WK_DIR/html
+   if [ ! -d "$RPT_DIR" ]; then
+      mkdir $RPT_DIR
    fi
+   cp $XFM_DIR/clair_table.html $RPT_DIR/vuln-table-$REPO-$BRANCH-$VER.html
 
-   cd html
-   cp $CLAIR_DIR/template_index.html index.html
-   sed -i "s/project/$REPO-$BRANCH-$VER/g" index.html
-   sed -i "s/linter_url//g" index.html
-   ln -f $PROJ_DIR/clair_html_file.html $REPO-$BRANCH-$VER-table.html
+   # Add link to new vuln file into index.html file:
+   $CLAIR_RPT/index.html
+   RPT_DIR="$CLAIR_RPT/$REPO-$BRANCH-$VER"
+
+   # >>---> ADD LINK HERE
+
+   # Clean up
+   cd $XFM_DIR/..
+   tar -czvf $REPO-$BRANCH-$VER.tar.gz $REPO-$BRANCH-$VER/
+   mv $REPO-$BRANCH-$VER.tar.gz $REPO-$BRANCH-$VER/
+   cd $REPO-$BRANCH-$VER/
+   rm -f clair_* clean* vuln*
 }
