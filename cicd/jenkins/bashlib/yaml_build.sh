478d80cd344c2e9083a3c7b71896c49dd11acf44
Chuck Bennett <cnbennett3@gmail.com>
2020-12-23 13:13:05 -0500

    Code improvements.

--- cicd/jenkins/bashlib/yaml_build.sh
+++ cicd/jenkins/bashlib/yaml_build.sh
@@ -35,18 +35,19 @@ function check_prereqs ()
    local -r ver_major=4
    local -r ver_minor=3
 
-   echo "check_prereqs"
-   echo "Checking pre-requisites for build . . ."
+   echo "${FUNCNAME[0]}"
+   echo "${FUNCNAME[0]}: Checking pre-requisites for build . . ."
    bash_ver=$(bash --version | head -1 | cut -d ' ' -f4)
    arr_bash_ver=(${bash_ver//./ })
 
    if [ ${arr_bash_ver[0]} -le $ver_major -a \
         ${arr_bash_ver[1]} -le $ver_minor ]; then
       echo "Insufficient bash version for build: [$bash_ver]"
-      exit 1
-   else
-      echo "Bash version is $bash_ver. Everything is cool."
+      return 1
    fi
+
+   echo "Bash version is $bash_ver. Everything's cool."
+   return 0
 }
 
 
@@ -58,9 +59,9 @@ function init_build ()
    local -r req_test=$3
    local -n func_arr=${4}
 
-   echo "init_build: $req_prebuild $req_build $req_test ${func_arr[@]}"
-   echo "Initializing build . . ."
-   echo "Setting up functions array . . ."
+   echo "${FUNCNAME[0]} $req_prebuild $req_build $req_test ${func_arr[@]}"
+   echo "${FUNCNAME[0]}: Initializing build . . ."
+   echo "${FUNCNAME[0]}: Setting up functions array . . ."
    request_arr=($req_prebuild $req_build $req_test)
    for index in {0..2}; do
       if [ ${request_arr[$index]} != "default" ]; then
@@ -69,22 +70,24 @@ function init_build ()
       fi
    done
 
-   echo "Fetching version library . . ."
+   echo "${FUNCNAME[0]}: Fetching version library . . ."
    VERSION_LIB_URL="https://raw.githubusercontent.com/helxplatform/devops/master/cicd/jenkins/bashlib/version_lib.sh"
    curl $VERSION_LIB_URL > version_lib.sh
    . ./version_lib.sh
 
-   echo "Fetching curl library . . ."
+   echo "${FUNCNAME[0]}: Fetching clair library . . ."
    CLAIR_LIB_URL="https://raw.githubusercontent.com/helxplatform/devops/master/cicd/jenkins/bashlib/clair_lib.sh"
    curl $CLAIR_LIB_URL > clair_lib.sh
    . ./clair_lib.sh
 
-   echo "Fetching custom libs . . ."
+   # Fetch custom prebuild function lib here if there are ever any custom prebuild functions
+
+   echo "${FUNCNAME[0]}: Fetching custom build function lib . . ."
    CUSTOM_BUILD_LIB_URL="https://raw.githubusercontent.com/helxplatform/devops/master/cicd/jenkins/bashlib/custom_build_lib.sh"
    curl $CUSTOM_BUILD_LIB_URL > custom_build_lib.sh
    . ./custom_build_lib.sh
 
-   echo "Fetching custom libs . . ."
+   echo "${FUNCNAME[0]}: Fetching custom test function lib . . ."
    CUSTOM_TEST_LIB_URL="https://raw.githubusercontent.com/helxplatform/devops/master/cicd/jenkins/bashlib/custom_test_lib.sh"
    curl $CUSTOM_TEST_LIB_URL > custom_test_lib.sh
    . ./custom_test_lib.sh
@@ -118,6 +121,13 @@ function prebuild ()
 
  #  echo "prebuild: $org $repo $branch $version_file $build_args"
    incr_version $version_file
+    rc=$?
+    case $rc in
+        0) ;;
+        255) echo "${FUNCNAME[0]}: Version file not found, exiting."; return $rc;;
+        *) echo "${FUNCNAME[0]}: Unknown error"; return $rc;;
+    esac
+
    local ver=$(get_version $version_file)
    echo "$ver" >&2
    local tags=$(get_tags $branch $ver)
@@ -143,8 +153,8 @@ function build ()
    local docker_path=$5
    local docker_fn=$6
 
-   echo "build: $org $repo $branch $build_args $tag1 $tag2 $app1_path $docker_path $docker_fn"
-   echo "Building app . . ."
+   echo "${FUNCNAME[0]} $org $repo $branch $build_args $tag1 $tag2 $app1_path $docker_path $docker_fn"
+   echo "${FUNCNAME[0]}: Building app . . ."
    if [ "$build_args" ==  "null" ]; then unset build_args; fi
    if [ "$docker_fn" == "null" ]; then docker_fn="Dockerfile"; fi
    if [ "$app1_path" != "." ]; then cd $app1_path; fi
@@ -173,9 +183,9 @@ function unit_test ()
    local cmd_args=$1
    local -r datafile=$2
 
-   echo -n "unit_test: $org $repo1 $repo2 $repo1_url $repo2_url $repo1_req_path $repo2_req_path "
+   echo -n "${FUNCNAME[0]} $org $repo1 $repo2 $repo1_url $repo2_url $repo1_req_path $repo2_req_path "
    echo "$branch $ver $tag1 $repo2_app_home $cmd_path $cmd_args $datafile"
-   echo "Executing unit tests . . ."
+   echo "${FUNCNAME[0]}: Executing unit tests . . ."
    if [ $cmd_path != "null" ]; then
       pwd
       ls -l
@@ -200,7 +210,7 @@ function unit_test ()
       else
          full_cmd_args="$cmd_args"
       fi
-      echo "Invoking test with cmd_path [$cmd_path] and cmd_args [$full_cmd_args]"
+      echo "${FUNCNAME[0]}: Invoking test with cmd_path [$cmd_path] and cmd_args [$full_cmd_args]"
       $cmd_path $full_cmd_args
    else
       true
@@ -216,8 +226,8 @@ function push ()
    local -r tag1=$3
    local -r tag2=$4
 
-   echo "push: $org $repo $tag1 $tag2"
-   echo "Pushing image to DockerHub . . ."
+   echo "${FUNCNAME[0]}: $org $repo $tag1 $tag2"
+   echo "${FUNCNAME[0]}: Pushing image to DockerHub . . ."
    docker push $org/$repo:$tag1
    docker push $org/$repo:$tag2
 }
@@ -232,15 +242,15 @@ function security_scan ()
    local ver=$4
    local tag=$5
 
-   echo "security_scan: $org $repo $tag"
+   echo "${FUNCNAME[0]}: $org $repo $tag"
    echo "Scanning image for security issues . . ."
    scan_clair "$org" "$repo" "$tag"  || true
    if [ $? -ne 0 ]
    then
-      echo "Skipping clair postprocessing."
+      echo "${FUNCNAME[0]}: Skipping clair postprocessing."
       return 1
    else
-      echo "postprocessing clair output" 
+      echo "${FUNCNAME[0]}: postprocessing clair output"
       postprocess_clair_output "$org" "$repo" "$branch" "$ver" "$tag" "Medium" || true
    fi
 }
@@ -252,8 +262,8 @@ function cleanup ()
    local org=$1
    local repo=$2
 
-   echo "cleanup: $org $repo"
-   echo "Cleaning up docker images . . ."
+   echo "${FUNCNAME[0]} $org $repo"
+   echo "${FUNCNAME[0]}: Cleaning up docker images . . ."
    docker images -a | grep "$org/$repo" | awk '{print $3}' | xargs docker rmi -f || true
 }
 
@@ -289,10 +299,10 @@ function build_app ()
    local -r CLAIR_THRESHOLD=1 # ""
    local -r CLAIR_WHITELIST=2 # ""
 
-   echo "build_app starting."
+   echo "${FUNCNAME[0]} starting."
 
    # Read YAML into arrays for processing 
-   echo "Reading yaml file . . ."
+   echo "${FUNCNAME[0]}: Reading yaml file . . ."
    local code_array=(`yq read -X $project.yaml 'code.*'`)
    local docker_array=(`yq read -X $project.yaml 'docker.*'`)
    local test_array=(`yq read -X $project.yaml 'test.*'`)
@@ -330,8 +340,13 @@ function build_app ()
    local -r REPO2_APP_HOME="$WS/$REPO2_APPDIR"
    local -r VERSION_FILE="$JENKINS_HOME/jobs/$project/version/$BRANCH/ver"
 
-   echo "Invoking check_prereqs"
+   echo "${FUNCNAME[0]}: Invoking check_prereqs"
    check_prereqs
+   if [ $? -ne 0 ]
+   then
+     echo "${FUNCNAME[0]}: check_prereqs failed, Exiting build." >&2
+     exit 1
+   fi
 
    # Init function array to the default build/test functions.
    func_array=(prebuild build unit_test)
@@ -345,14 +360,20 @@ function build_app ()
    #   in the default functions and hopefully increases maintainability.
    # For simplicity, the custom functions must use the same parameter list as
    #   the default functions.
-   echo "Requested build/test functions: $REQ_PREBUILD_FUNC, $REQ_BUILD_FUNC, $REQ_TEST_FUNC"
-   echo "Invoking init_build $REQ_PREBUILD_FUNC $REQ_BUILD_FUNC $REQ_TEST_FUNC func_array"
+   echo "${FUNCNAME[0]}: Requested build/test functions: $REQ_PREBUILD_FUNC, $REQ_BUILD_FUNC, $REQ_TEST_FUNC"
+   echo "${FUNCNAME[0]}: Invoking init_build $REQ_PREBUILD_FUNC $REQ_BUILD_FUNC $REQ_TEST_FUNC func_array"
    init_build $REQ_PREBUILD_FUNC $REQ_BUILD_FUNC $REQ_TEST_FUNC func_array
-   echo "Post-init_build function array: ${func_array}[@]"
+   echo "Post-init_build function array: ${func_array[@]}"
 
    # Invoke prebuild:
-   echo "Invoking ${func_array[$PREBUILD]} $ORG $REPO1 $BRANCH $VERSION_FILE $BUILD_ARGS"
+   echo "${FUNCNAME[0]}: Invoking ${func_array[$PREBUILD]} $ORG $REPO1 $BRANCH $VERSION_FILE $BUILD_ARGS"
    local build_info=$(${func_array[$PREBUILD]} $ORG $REPO1 $BRANCH $VERSION_FILE $BUILD_ARGS)
+   rc=$?
+   case $rc in
+      0) ;;
+      255) echo "${FUNCNAME[0]}: prebuild failed, Exiting."; exit 2;;
+      *) echo "${FUNCNAME[0]}: Unknown error, Exiting."; exit 3;;
+   esac
 
    # Process array of returned vars for tags and version:
    local build_array=(${build_info//:/ })
@@ -361,38 +382,38 @@ function build_app ()
    local -r VER=${build_array[2]}
 
    # Invoke build:
-   echo "Invoking ${func_array[$BUILD]} $ORG $REPO1 $BRANCH [$BUILD_ARGS] $TAG1 $TAG2 $APP_HOME $DOCKER_DIR1 $DOCKER_FN"
+   echo "${FUNCNAME[0]}: Invoking ${func_array[$BUILD]} $ORG $REPO1 $BRANCH [$BUILD_ARGS] $TAG1 $TAG2 $APP_HOME $DOCKER_DIR1 $DOCKER_FN"
    ${func_array[$BUILD]} $ORG $REPO1 $BRANCH "$BUILD_ARGS" $TAG1 $TAG2 $APP_HOME $DOCKER_DIR1 $DOCKER_FN
    if [ $? -ne 0 ]
    then
-     echo "Build failed, skipping tests and not pushing to Dockerhub." >&2
-     exit 2
+     echo "build_app: Build failed, skipping tests and not pushing to Dockerhub. Exiting." >&2
+     exit 4
    fi
 
    # Invoke unit tests:
-   echo -n "Invoking ${func_array[$UNIT_TEST]} $ORG $REPO1 $REPO2 $REPO1_URL $REPO2_URL "
+   echo -n "${FUNCNAME[0]}: Invoking ${func_array[$UNIT_TEST]} $ORG $REPO1 $REPO2 $REPO1_URL $REPO2_URL "
    echo "$REPO1_REQ_PATH $REPO2_REQ_PATH $BRANCH $VER $TAG1 $APP_HOME $REPO2_APP_HOME $CMD_PATH $CMD_ARGS $DATAFILE"
    ${func_array[$UNIT_TEST]} $ORG $REPO1 $REPO2 $REPO1_URL $REPO2_URL $REPO1_REQ_PATH \
                 $REPO2_REQ_PATH $BRANCH $VER $TAG1 $APP_HOME $REPO2_APP_HOME $CMD_PATH "$CMD_ARGS" $DATAFILE
    if [ $? -ne 0 ]
    then
-      echo "Unit tests failed, not pushing to DockerHub." >&2
-      exit 3
+      echo "${FUNCNAME[0]}: Unit tests failed, not pushing to DockerHub. Exiting." >&2
+      exit 5
    fi
 
    # Push to DockerHub
-   echo "Invoking push $ORG $REPO1 $TAG1 $TAG2"
+   echo "${FUNCNAME[0]}: Invoking push $ORG $REPO1 $TAG1 $TAG2"
    push $ORG $REPO1 $TAG1 $TAG2
 
    # Do clair scanning
-   echo "Invoking security_scan $ORG $REPO1 $TAG1"
+   echo "${FUNCNAME[0]}: Invoking security_scan $ORG $REPO1 $TAG1"
    security_scan $ORG $REPO1 $BRANCH $VER $TAG1
 
    # Clean up build artifacts
-   echo "Invoking cleanup $ORG $REPO1"
+   echo "${FUNCNAME[0]}: Invoking cleanup $ORG $REPO1"
    cleanup $ORG $REPO1
 
-   echo "Done."
+   echo "${FUNCNAME[0]}: Done."
 }
 
 build_project=$1
